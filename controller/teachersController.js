affee8a4be558567d2c14ea0b07b8d36406a34e4
Peter2s <petersalahsamir@gmail.com>
2023-02-21 16:43:05 +0200

    add Models ,  CRUD  + add auto inc id , hash pass

--- controller/teachersController.js
+++ controller/teachersController.js
@@ -1,13 +1,62 @@
-exports.getAllTeachers = (req,res)=>{
-    res.status(200).json({data:[]});
+const mongoose = require("mongoose");
+require("../model/teacherModel");
+const TeachersSchema = mongoose.model("teacher");
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+exports.getAllTeachers = (req,res)=>{
+    TeachersSchema.find({})
+    .then(data=>res.status(200).json({data}))
+    .catch(err=>next(err))
 }
-exports.addTeacher = (req,res)=>{
-    res.status(201).json({data:[]});
+exports.getTeacherById = (req,res)=>{
+    TeachersSchema.findById(req.params._id)
+    .then(data=> res.status(200).json({data}))
+    .catch(error=>next(error))
 }
-exports.updateTeacher = (req,res)=>{
-    res.status(200).json({data:[]});
+exports.addTeacher = async (req,res,next)=>{
+    const passwordHashed = await bcrypt.hash(req.body.password, saltRounds)
+    new TeachersSchema({
+        _id:req.body._id,
+        fullname:req.body.fullname,
+        password:passwordHashed,
+        email:req.body.email,
+        image:req.body.image
+    }).save()
+    .then(data=> res.status(201).json({data}))
+    .catch(error=>next(error))
 }
-exports.deleteTeacher = (req,res)=>{
-    res.status(200).json({data:[]});
+exports.updateTeacher = async (req,res,next)=>{
+    const passwordHashed = await bcrypt.hash(req.body.password, saltRounds)
+    TeachersSchema.updateOne(
+        {
+            _id:req.params._id
+        },
+        {
+            $set:{
+                fullname:req.body.fullname,
+                password:passwordHashed,
+                email:req.body.email,
+                image:req.body.image
+            }
+        }
+        )
+    .then(data =>{
+        if(data.modifiedCount == 0){
+            throw new Error("not found this teacher")
+        }
+        res.status(200).json({data});
+    })
+    .catch(error=>next(error))
+}
+exports.deleteTeacher = (req,res,next)=>{
+    TeachersSchema.deleteOne({_id:req.params._id})
+    .then(data=>{
+        if(data.deletedCount > 0)
+            res.status(200).json({data})
+        else
+            throw new Error("can't delete not exist teacher ")
+    })
+    .catch(error=>next(error))
 }
